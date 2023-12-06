9b5956b0c9f92c904a9ab926dd407963d2fee531
Nicolas Gelin <nicolas.gelin@edu.univ-fcomte.fr>
2023-12-06 11:49:37 +0100

    correction ficheDesign

--- partials/ficheDesign.php
+++ partials/ficheDesign.php
@@ -2,11 +2,9 @@
     <div src="" alt="" class="ficheDesign__image"></div>
     <h3 class="ficheDesign__name">Nom produit</h3>
     <h4 class="ficheDesign__nameDesigner">By Nom designer</h3>
-    <div class="ficheDesign__divSee">
         <button class="ficheDesign__seeMore">
             <a class="ficheDesign__link" href="#">
                 Voir tous les produits
             </a>
         </button>
-    </div>
-</div>
+</div> 
